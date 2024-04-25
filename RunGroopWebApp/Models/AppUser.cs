329b521d98cd2b15faf39b8fed6d35e29fd49192
1anKoz <jankozinski@wp.pl>
2024-04-25 15:59:42 +0200

    first commit

---
+++ RunGroopWebApp/Models/AppUser.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace RunGroopWebApp.Models
+{
+    public class AppUser
+    {
+        public int? Pace {  get; set; }
+        public int Mileage { get; set; }
+        public Adress? Adress { get; set; }
+
+    }
+}
